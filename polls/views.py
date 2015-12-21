4f438bfe96e3b63b5fa9461957d9c2abe25901b7
hoorn91 <richard.hoorn91@gmail.com>
2015-12-21 23:40:18 +0100

    asd

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
-from django.template import RequestContext, loader
-from .models import Question, Choice
-from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.views import generic
+
+from .models import Choice, Question
 
 
 class IndexView(generic.ListView):
